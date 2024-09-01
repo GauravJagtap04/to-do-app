9ea65efb7bd3be0ae4d4cc675b3dcf6ed1362879
GauravJagtap04 <gauravjagtap765@gmail.com>
2024-09-01 23:42:41 +0530

    day 4 progress

--- todo.js
+++ todo.js
@@ -1,10 +1,16 @@
 const inputPrio = document.getElementById("priority");
 const inputTask = document.getElementById("input-task");
+const completedTask = document.getElementById("c1-13");
 const submitBtn = document.getElementById("submit-btn");
 const outputFldLst = document.getElementById("output-field-list");
+const completedOutputFldLst = document.getElementById("output-field-list-completed");
 
 const taskData = JSON.parse(localStorage.getItem("data")) || [];
-const currentTask = {};
+const completedTaskData = JSON.parse(localStorage.getItem("completed_data")) || [];
+
+let currentTask = {};
+
+const today = new Date();
 
 const addTask = () => {
     const taskValue = inputTask.value.trim();
@@ -19,6 +25,8 @@ const addTask = () => {
         id: `${taskValue.toLowerCase().split(" ").join("-")}-${Date.now()}`,
         task: taskValue,
         priority: priorityValue,
+        date: `${today.toLocaleDateString()}`,
+        minutes: `${today.toLocaleTimeString()}`,
     };
 
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
@@ -40,7 +48,7 @@ const updateOutputFldLst = () => {
     outputFldLst.innerHTML = "";
 
     taskData.forEach(
-        ({ id, task, priority }) => {
+        ({ id, task, priority, date, minutes }) => {
             let priorityColor;
             if (priority === "L") priorityColor = "var(--low-priority)";
             else if (priority === "M") priorityColor = "var(--med-priority)";
@@ -48,11 +56,11 @@ const updateOutputFldLst = () => {
 
             outputFldLst.innerHTML += `
                 <li id="${id}">
-					<table style="box-shadow: 0px 0px 10px ${priorityColor};">
+					<table style="box-shadow: 0px 0px 10px ${priorityColor};" title="${date + " " + minutes}">
 						<tr>
 							<td>
 								<div class="checkbox-wrapper-13">
-									<input id="c1-13" type="checkbox">
+									<input id="c1-13" type="checkbox" onchange="addCompleted('${id}')">
 								</div>
 							</td>
 							<td>${task}</td>
@@ -65,7 +73,35 @@ const updateOutputFldLst = () => {
     );
 };
 
-const _deleteTask = (taskId) => {
+const updateCompletedOutputFldLst = () => {
+    if (completedTaskData.length >= 1) {
+        completedOutputFldLst.innerHTML = "<h2>Completed tasks</h2>";
+    } else {
+        completedOutputFldLst.innerHTML = "";
+    }
+
+    completedTaskData.forEach(
+        ({ id, task, priority, date, minutes }) => {
+            let priorityColor;
+            if (priority === "L") priorityColor = "var(--low-priority)";
+            else if (priority === "M") priorityColor = "var(--med-priority)";
+            else if (priority === "H") priorityColor = "var(--high-priority)";
+
+            completedOutputFldLst.innerHTML += `
+                <li id="${id}">
+					<table style="box-shadow: 0px 0px 10px ${priorityColor};">
+						<tr>
+							<td style="width: 95%; text-align: center; margin: 0; padding: 0;" title="${minutes}">${task}</td>
+							<td style="width: 5%; text-align: right; margin: 0; padding: 0;"><img id="cancel" onclick="deleteCompletedTask('${id}')" src="images/x.png" alt="cancel"></td>
+						</tr>
+					</table>
+				</li>
+            `;
+        }
+    );
+};
+
+const deleteTask = (taskId) => {
     const dataArrIndex = taskData.findIndex(
         (item) => item.id === taskId
     );
@@ -76,6 +112,45 @@ const _deleteTask = (taskId) => {
         localStorage.setItem("data", JSON.stringify(taskData));
     }
 };
+
+const deleteCompletedTask = (taskId) => {
+    const dataArrIndex = completedTaskData.findIndex(
+        (item) => item.id === taskId
+    );
+
+    if (dataArrIndex !== -1) {
+        completedTaskData.splice(dataArrIndex, 1);
+        document.getElementById(taskId).remove();
+        localStorage.setItem("completed_data", JSON.stringify(completedTaskData));
+    }
+
+    if (completedTaskData < 1) {
+        completedOutputFldLst.innerHTML = "";
+    }
+};
+
+const addCompleted = (taskId) => {
+    const dataArrIndex = taskData.findIndex(item => item.id === taskId);
+
+
+
+    if (dataArrIndex !== -1) {
+        const completedTaskNew = taskData.splice(dataArrIndex, 1)[0];
+
+        const now = new Date();
+        completedTaskNew.date = now.toLocaleDateString();
+        completedTaskNew.minutes = now.toLocaleTimeString();
+
+        completedTaskData.unshift(completedTaskNew);
+
+        localStorage.setItem("data", JSON.stringify(taskData));
+        localStorage.setItem("completed_data", JSON.stringify(completedTaskData));
+
+        updateOutputFldLst();
+        updateCompletedOutputFldLst();
+    }
+};
+
 
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
@@ -91,6 +166,8 @@ submitBtn.addEventListener('click', (e) => {
 
 document.addEventListener('DOMContentLoaded', () => {
     updateOutputFldLst();
+    updateCompletedOutputFldLst();
 });
 
 updateOutputFldLst();
+updateCompletedOutputFldLst();
